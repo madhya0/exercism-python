992ac43a01d928d8ee120e4bbf0f638d7ad22ff2
josealfonsoescobarmejia@gmail.com <josealfonsoescobarmejia@gmail.com>
2020-12-09 19:24:47 -0600

    agrege archivos

---
+++ two-fer/two_fer.py
@@ -0,0 +1,9 @@
+def two_fer(nombre = ""):
+
+
+    if nombre == "Bob":
+        return "One for Bob, one for me."
+    elif nombre == "Alice":
+        return'One for Alice, one for me.'
+    elif nombre == "":
+        return "One for you, one for me."
